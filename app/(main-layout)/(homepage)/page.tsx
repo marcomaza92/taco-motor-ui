73d817a12f546847c234ce6d15e64130c0543465
marcomaza92 <marcoagustinmaza@gmail.com>
2025-02-23 23:25:10 -0300

    fix: remove unnecessary markup

--- app/(main-layout)/(homepage)/page.tsx
+++ app/(main-layout)/(homepage)/page.tsx
@@ -3,11 +3,9 @@ import styles from "./page.module.css";
 const Homepage = () => {
   return (
     <section className={styles.fold}>
-      <div>
-        <h1>Welcome to the Taco Motor UI</h1>
-        <h3>Here you&apos;ll find a sample app that consumes an API</h3>
-        <p>Navigate with the links on the navbar above</p>
-      </div>
+      <h1>Welcome to the Taco Motor UI</h1>
+      <h3>Here you&apos;ll find a sample app that consumes an API</h3>
+      <p>Navigate with the links on the navbar above</p>
     </section>
   );
 };
